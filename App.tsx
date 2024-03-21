77d3faf0200de8aa984a7e0e8522539bdc832d71
Nisha Chavan <nisha.chavan@lekab.com>
2024-03-21 06:43:27 +0100

    router set

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
+import {Container} from "react-bootstrap";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+        <header className="App-header">
 
-      </header>
+        </header>
+        <Container className={"my-4"}>
+            <Routes>
+                <Route />
+            </Routes>
+        </Container>
+        <footer className={"App-footer"}>
+
+        </footer>
     </div>
   );
 }
