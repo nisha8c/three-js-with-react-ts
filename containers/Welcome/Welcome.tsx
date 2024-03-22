876dd65b65b426bbb761fc82a91a74f4ea746d1a
Nisha Chavan <nisha.chavan@lekab.com>
2024-03-22 12:22:33 +0100

    GRID LAYOUT TRYING

--- containers/Welcome/Welcome.tsx
+++ containers/Welcome/Welcome.tsx
@@ -1,16 +1,32 @@
-import {Button, Container, Stack} from "react-bootstrap";
+import {Button, Col, Container, Row, Stack} from "react-bootstrap";
 import './Welcome.scss';
 import {Link} from "react-router-dom";
+import healthyFood1 from '../../Images/healthy-pumpkin-risotto-lunch.jpg';
 
 function Welcome() {
     console.log('Welcome component is being rendered');
     return (
         <Stack className={"welcome-main"}>
             <Container className={"white-font-color"}>
-                <h1 className={"white-font-color"}>Welcome</h1>
-                <Link to={'/weekly-menu'}><Button>Explore Menu</Button></Link>
+                <Row className={""}>
+                    <Col xs={12} md={8}>
+                        <h1 className={"white-font-color"}>
+                            Delicious Catering & Food Distribution Services
+                        </h1>
+                        <h6 className={"white-font-color"}>
+                            Explore our wide range of food options for any event or occasion!
+                        </h6>
+                        <Link to={'/weekly-menu'}><Button>Explore Menu</Button></Link>
+                    </Col>
+                    <Col xs={6} md={4} className={"image-column"}>
+                        <img
+                            src={healthyFood1}
+                            alt={"healthy-food1"}/>
+                    </Col>
+                </Row>
             </Container>
         </Stack>
     );
 }
+
 export default Welcome;
