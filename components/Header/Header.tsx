73e02e54c2c913703e0523b8eccffce9a25fe265
Nisha Chavan <nisha.chavan@lekab.com>
2024-03-22 11:47:44 +0100

    HEADER BASIC done

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,37 @@
-import './Header.scss'
-function Header() {
-    return(
-        <header className={"header"}>Header</header>
+import React from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import './Header.scss';
+// import logo from '../../constants/White on Transparent.png'
+
+const Header: React.FC = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg={"black"} variant="dark">
+            <Navbar.Brand href="/">
+                Tim's Kitchen
+            </Navbar.Brand>
+
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#features">Link 1</Nav.Link>
+                    <Nav.Link href="#pricing">Link 2</Nav.Link>
+                    <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                <Nav>
+                    <Nav.Link href="#deets">User Link 1</Nav.Link>
+                    <Nav.Link eventKey={2} href="#memes">
+                        User Link 2
+                    </Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
+
 export default Header;
