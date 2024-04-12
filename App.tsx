631cca83704dda1214e0536c89887699c665cd63
Nisha Chavan <nisha.chavan@lekab.com>
2024-04-12 11:13:12 +0200

    getWeekMenus

--- App.tsx
+++ App.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Navigate, Route, Routes, useNavigate} from "react-router-dom";
 import {Welcome, WeeklyMenu} from "./containers";
 import {ClerkProvider, SignIn, SignUp} from "@clerk/clerk-react";
 import WithSignInProtectionHOC from "./HOC/WithSignInProtectionHOC";
 import {Footer, Header} from "./components";
+import {IWeeklyMenuProps} from "./interfaces/interfaces";
 
+const server_port = process.env.SERVER_PORT || 8000;
 if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
     throw new Error("Missing Publishable Key")
 }
 const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
 
 function App() {
-  return (
+  const [ weekMenus, setWeekMenus ] = useState<IWeeklyMenuProps[]>();
+    useEffect(() => {
+        const getWeekMenus = async() => {
+            const response = await fetch (`http://localhost:${server_port}/api/weekMenus`);
+            const data = await response.json();
+            setWeekMenus(data);
+            console.log(weekMenus);
+        };
+        getWeekMenus();
+    }, []);
+
+    return (
       <>
           <ClerkProviderWithRoutes />
           <Footer />
